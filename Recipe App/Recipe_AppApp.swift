5331180fba14373ede5ce96fe97858c9e90739fc
Thongchai Subsaidee <chanoktrue@me.com>
2021-09-22 04:52:55 +0700

    Initial Commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Thongchai Subsaidee on 22/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
